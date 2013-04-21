d30ccf16cc26e2a904eaaf1ca97720b9923c161c
Robert Silve <robert@silve.net>
2013-04-21 21:21:07 +0200

    add error message on editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,15 @@ define(['angular', 'grid', 'chords'], function () {
 				if (value === undefined || value === "") {
 					return;
 				}
-				$(attrs.chordsRender).chords({json: parser.parse(value)});
+				try {
+				  var json = parser.parse(value)
+				  $(attrs.chordsRender).chords({json: json});
+				  $(attrs.chordsMessage).empty().hide();
+				} catch (e) {
+				  var m = e.message.split("\n");
+				  var s = m[0] + "\n" + m[1].substr(Math.max(0, m[1].length - 5));
+				  $(attrs.chordsMessage).text(s).show();	
+				}
       		});
 		}
 	});
